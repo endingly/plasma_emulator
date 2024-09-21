d9a1808857f112fc1c15e609c5c9db52bb84832b
endingly <jiasongdeng@qq.com>
2024-09-21 17:23:44 +0800

    fix error parser function

--- submod/chempars/src/equation.cpp
+++ submod/chempars/src/equation.cpp
@@ -10,25 +10,26 @@ gds::chempars::Equation::Equation(const std::string& str) {
   // split the string by ","
   auto comma_index0 = str.find_first_of(",");
   auto equation_str = str.substr(0, comma_index0);
-  auto type_str = str.substr(comma_index0 + 1, str.size() - comma_index0 - 1);
+  auto type_str     = str.substr(comma_index0 + 1, str.size() - comma_index0 - 1);
 
   auto index = equation_str.find("->");
   if (index == std::string::npos) {
     throw std::invalid_argument("Invalid equation: " + equation_str);
   }
   auto reactants_str = equation_str.substr(0, index);
-  auto products_str =
-      equation_str.substr(index + 2, equation_str.size() - index - 2);
+  auto products_str  = equation_str.substr(index + 2, equation_str.size() - index - 2);
 
   auto reactants_vec = parse_equation_str(reactants_str);
   auto products_vec  = parse_equation_str(products_str);
 
   // generate the equation object
   for (auto&& item : reactants_vec) {
-    reactants.push_back(item);
+    Partical p(item);
+    reactants.push_back(p);
   }
   for (auto&& item : products_vec) {
-    products.push_back(item);
+    Partical p(item);
+    products.push_back(p);
   }
 
   // parse the type string
@@ -38,8 +39,7 @@ gds::chempars::Equation::Equation(const std::string& str) {
   this->eq_id = id_counter++;
 }
 
-std::vector<std::string> gds::chempars::Equation::parse_equation_str(
-    const std::string& equation_str) {
+std::vector<std::string> gds::chempars::Equation::parse_equation_str(const std::string& equation_str) {
   auto result_vec = std::vector<std::string>();
   int  index      = 0;
   // parse equation string
@@ -55,15 +55,13 @@ std::vector<std::string> gds::chempars::Equation::parse_equation_str(
     target = equation_str.find("+", index);
   }
   if (index < equation_str.size()) {
-    result_vec.push_back(
-        equation_str.substr(index, equation_str.size() - index));
+    result_vec.push_back(equation_str.substr(index, equation_str.size() - index));
   }
 
   return result_vec;
 }
 
-gds::chempars::EquationType gds::chempars::Equation::parse_equation_type(
-    const std::string& type_str) {
+gds::chempars::EquationType gds::chempars::Equation::parse_equation_type(const std::string& type_str) {
   if (type_str.find("ionization") != std::string::npos)
     return EquationType::ionization;
   else if (type_str.find("elastic") != std::string::npos)
@@ -75,8 +73,7 @@ gds::chempars::EquationType gds::chempars::Equation::parse_equation_type(
   else if (type_str.find("fadding_excitation") != std::string::npos)
     return EquationType::fadding_excitation;
   else {
-    std::cerr << "Parsing error, invalid equation type: " << type_str
-              << std::endl;
+    std::cerr << "Parsing error, invalid equation type: " << type_str << std::endl;
     exit(1);
   }
 }
