b89082304972f5c0d1e24f623f369b5c09197be9
endingly <jiasongdeng@qq.com>
2024-09-21 12:11:06 +0800

    add units to config and some physic constant

--- submod/core/test/test_units.cpp
+++ submod/core/test/test_units.cpp
@@ -1,7 +1,16 @@
 #include <gtest/gtest.h>
 
+#include "config.hpp"
 #include "physic_constant.hpp"
 
 TEST(UnitsTest, TestUnits) {
   std::cout << gds::physic_constant::T_e << std::endl;
+  std::cout << gds::physic_constant::cubic_centimeter_per_second_t(1.0) << std::endl;
+}
+
+TEST(UnitsTest, TestConfig) {
+  std::cout << gds::config::xmax << std::endl;
+  std::cout << gds::config::ymax << std::endl;
+  std::cout << gds::config::pressure << std::endl;
+  std::cout << gds::config::voltage << std::endl;
 }
