cd5e52e909b287363ccff1188e6ab13bfe6055dd
endingly <jiasongdeng@qq.com>
2024-10-23 21:31:43 +0800

    fix bug of parsing csv file

--- submod/core/src/matrix_helper.cpp
+++ submod/core/src/matrix_helper.cpp
@@ -3,6 +3,7 @@
 #include <rapidcsv.h>
 
 #include <algorithm>
+#include <cstdint>
 #include <string>
 
 #include "matrix.hpp"
@@ -10,17 +11,17 @@
 namespace gds::core {
 
 Matrix MatrixHelper::load_matrix_from_CSV(const std::string &file_name) {
-  rapidcsv::Document doc(file_name, rapidcsv::LabelParams(0, 0), rapidcsv::SeparatorParams(' ', 0));
-  auto               rows = doc.GetRowCount() + 1;
+  rapidcsv::Document doc(file_name, rapidcsv::LabelParams(-1, -1), rapidcsv::SeparatorParams(' ', 0));
+  auto               rows = doc.GetRowCount();
   auto               row0 = doc.GetRow<std::string>(0);
   row0.erase(std::remove(row0.begin(), row0.end(), ""), row0.end());
   auto   cols = row0.size();
   Matrix matrix(rows, cols);
-  for (int i = -1; i < int(rows - 1); i++) {
+  for (uint32_t i = 0; i < rows; i++) {
     auto tr = doc.GetRow<std::string>(i);
     tr.erase(std::remove(tr.begin(), tr.end(), ""), tr.end());
     for (int j = 0; j < cols; j++) {
-      matrix(i + 1, j) = std::stod(tr[j]);
+      matrix(i, j) = std::stod(tr[j]);
     }
   }
   return matrix;
