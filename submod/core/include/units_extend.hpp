a0dee87b0c1fff828918c00cd8f1427c1f411195
endingly <jiasongdeng@qq.com>
2024-10-25 13:28:24 +0800

    add new units in new file

--- submod/core/include/units_extend.hpp
+++ submod/core/include/units_extend.hpp
@@ -1,19 +1,25 @@
 #pragma once
 #include <units.h>
+
+#include <ratio>
 #include <sstream>
-#include <concepts>
+
+namespace units {
+
+UNIT_ADD(volume, cubic_centimeter, cubic_centimeter, cc,
+         units::unit<std::ratio<1, 1000000>, units::volume::cubic_meter>);
+
+UNIT_ADD(reaction_rate, cubic_centimeter_per_second, cubic_centimeter_per_second, ccps,
+         compound_unit<inverse<volume::cc>, inverse<time::second>>);
+
+};  // namespace units
 
 namespace units::extend {
 
 template <typename T>
-concept Units = requires(T t, std::ostream& os) {
-  { t.abbreviation() } -> std::same_as<const char*>;
-  { t.name() } -> std::same_as<const char*>;
-  t.value();
-  { os << t } -> std::same_as<std::ostream&>;
-};
+concept UnitsLike = units::traits::is_unit_t<T>::value;
 
-template <Units T>
+template <UnitsLike T>
 inline std::string to_string(T t) {
   std::stringstream ss;
   ss << t;
