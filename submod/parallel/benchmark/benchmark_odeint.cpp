f3fe4e0941c2272c857d9896ae6ddb65d76d9c60
endingly <jiasongdeng@qq.com>
2024-10-28 23:51:59 +0800

    modify benchmark file

--- submod/parallel/benchmark/benchmark_odeint.cpp
+++ submod/parallel/benchmark/benchmark_odeint.cpp
@@ -11,22 +11,3 @@ static void odeint_pi(benchmark::State& state) {
   }
 }
 BENCHMARK(odeint_pi)->Unit(benchmark::kMicrosecond);
-
-static void odeint_pi_cuda(benchmark::State& state) {
-  constexpr uint64_t step_num  = 4194304;
-  constexpr double   start_x   = 0.0;
-  constexpr double   end_x     = 1.0;
-  constexpr double   step_size = (end_x - start_x) / step_num;
-
-  std::vector<double> x(step_num);
-  std::vector<double> y(step_num);
-  for (int i = 0; i < step_num; i++) {
-    x[i] = start_x + i * step_size;
-    y[i] = f(x[i]);
-  }
-
-  for (auto _ : state) {
-    gds::parallel::odeint::integrate(x, y);
-  }
-}
-BENCHMARK(odeint_pi_cuda)->Unit(benchmark::kMicrosecond);
